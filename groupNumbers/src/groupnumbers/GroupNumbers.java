afdf9c7de21fefa435113c986ff2303d3867bac2
Heinrich <hscheepers10@gmail.com>
2020-12-08 21:21:36 +0200

    Linked methods
    Can use methods for testing.

--- groupNumbers/src/groupnumbers/GroupNumbers.java
+++ groupNumbers/src/groupnumbers/GroupNumbers.java
@@ -10,19 +10,23 @@ public class GroupNumbers {
 
     //Variables.  
     public static String usrInput_a ;                                             //inout received from user.  
-    public static String usrInput_b = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";     //static string of numbers for test purposes. 
+//    public static String usrInput_b = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";     //static string of numbers for test purposes. 
     
     public static void main(String[] args) {
         //Scanner.  
-//        Scanner cin = new Scanner(System.in);
+        Scanner cin = new Scanner(System.in);
         
         //User interraction.  
-//        System.out.println("Enter numbers sepperated buy commas: ");
-//        usrInput_a = cin.next();
+        System.out.println("Enter numbers sepperated buy commas: ");
+        usrInput_a = cin.next();
         
+        //instance class sumNumber that implements NumberRangeSummarizer interface.  
         sumNumbers sum1 = new sumNumbers();
         
-        System.out.println(sum1.collect(usrInput_b));
+        //Test purposes.  
+        //Actual calling of methods will happen in sumNumbers class.  
+//        System.out.println(sum1.collect(usrInput_b));
+        System.out.println(sum1.summarizeCollection(sum1.collect(usrInput_a)));
         
         
 
--- groupNumbers/src/groupnumbers/sumNumbers.java
+++ groupNumbers/src/groupnumbers/sumNumbers.java
@@ -1,8 +1,10 @@
 package groupnumbers;
 
-import static groupnumbers.GroupNumbers.usrInput_b;
+import static groupnumbers.GroupNumbers.usrInput_a;
+//import static groupnumbers.GroupNumbers.usrInput_b;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,25 +14,47 @@ import java.util.stream.Collectors;
 
 public class sumNumbers implements NumberRangeSummarizer{
 
+    
+    //Getting user input.  
     @Override
     public Collection<Integer> collect(String input) {
-        input = usrInput_b;                             //Assigning input interface's String variable.  
+        input = usrInput_a;                                 //Assigning input interface's String variable.  
 
-        String[] arr = input.split(",");                //Converting String into Array by splitting ",".  
+        String[] arr = input.split(",");                    //Converting String into Array by splitting ",".  
 
-        List<Integer> intList = Arrays.stream(arr)      //Creating List by ....
+        List<Integer> intList = Arrays.stream(arr)          //Creating List by ....
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        Collections.sort(intList);                          //Added sort to sort data automatically.  
+                
+        //TODO: Duplicates ????
+        //TODO: Sort list through stream ????
+        //TODO: Change variables to compliment current setting.  
         
-        return intList;                                 //Returns collection of type Integer (Required).  
+        return intList;                                     //Returns collection of type Integer (Required).  
     }
 
+    
+    
+    
+    //Summarizing data.  
     @Override
-    public String summarizeCollection(Collection<Integer> input) {
+    public String summarizeCollection(Collection<Integer> input) {  
+        String res;                                    //Ressult variable of type String to be returned to main.
+        //TODO: decide whether to 'sout' in main or .this.
+        
+        
+        input = this.collect(usrInput_a);
+        
+        res = input.toString();
+        
+        
+        //////////////////LOGIC//////////////////////////
         
         
         
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        return res;
     }
     
 }
